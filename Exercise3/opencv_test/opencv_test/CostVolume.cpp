18cada165cb798cd6deb7a6805c6a8a9b99d3b64
Robin Melan <el_argentino23@hotmail.com>
2017-06-05 19:36:15 +0200

    Task 2

--- Exercise3/opencv_test/opencv_test/CostVolume.cpp
+++ Exercise3/opencv_test/opencv_test/CostVolume.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void CostVolume::computeCostVolume(const cv::Mat &imgLeft, const cv::Mat &imgRight, std::vector<cv::Mat>
 	&costVolumeLeft, std::vector<cv::Mat> &costVolumeRight, int windowSize, int maxDisp) {
 
+		
 	//printf("Width = %d, Height = %d", imgLeft.cols, imgLeft.rows);
 	
 	printf("Started computing CostVolume...\n");
@@ -28,7 +29,6 @@ try
 	* CIELab color space is now precomputed and stored in a separate mat
 	*/
 
-	
 	//precompute spatial distances
 	Mat1f distMap(windowSize, windowSize);
 	for (int k = 0; k < windowSize; k++) {
@@ -36,14 +36,14 @@ try
 			distMap.at<float>(k, l) = sqrt(pow((float)(k - w), 2) + pow((float)(l - w), 2)) / gamma_p;
 		}
 	}
-
+	
 	//Convert to CIELab
 	Mat3b imgLeft_LAB(width, height);
 	Mat3b imgRight_LAB(width, height);
 	cvtColor(imgLeft, imgLeft_LAB, CV_RGB2Lab);
 	cvtColor(imgRight, imgRight_LAB, CV_RGB2Lab);
 
-
+	
 	//d = Disparity
 	for (int d = 0; d < maxDisp; d++) {
 
@@ -52,110 +52,109 @@ try
 
 		//For each row of the image
 		for (int i = 0; i < height; i++) {
-
+			
+			// RGB
 			const Vec3b* l_pixel = imgLeft.ptr<Vec3b>(i); // point to first pixel in row
 			const Vec3b* r_pixel = imgRight.ptr<Vec3b>(i);
+
+			// Lab
 			Vec3b* l_pixel_LAB = imgLeft_LAB.ptr<Vec3b>(i);
 			Vec3b* r_pixel_LAB = imgRight_LAB.ptr<Vec3b>(i);
-
+			
 			//For each column of the image
 			for (int j = 0; j < width; j++) {
 				//Window
+				float sum_v = 0; //sum of values (weights * absdiff)
 				float sum_w = 0; //sum of weigths
-				float sum_v = 0; //sum of values (weight * absdiff)
-
 				
 				if (i + d < width) {
-					//Center Pixel colors
+					// RGB
 					Vec3b l_color = l_pixel[j + d];
 					Vec3b r_color = r_pixel[j];
-
+				
+					// Lab
 					Vec3b l_color_LAB = l_pixel_LAB[j + d];
 					Vec3b r_color_LAB = r_pixel_LAB[j];
 
-
-
 					//for each window row
 					for (int k = 0; k < windowSize; k++) {
-
+					
 						int y = i + k - w;
 						if (y >= 0 && y < height) {
 
-							//Row pointer in window
+							// RGB
 							const Vec3b* l_w_pixel = imgLeft.ptr<Vec3b>(i + k - w); // point to first pixel in row
 							const Vec3b* r_w_pixel = imgRight.ptr<Vec3b>(i + k - w);
+
+							//Lab
 							Vec3b* l_w_pixel_LAB = imgLeft_LAB.ptr<Vec3b>(i + k - w);
 							Vec3b* r_w_pixel_LAB = imgRight_LAB.ptr<Vec3b>(i + k - w);
 
 							//for each window col
 							for (int l = 0; l < windowSize; l++) {
-
-								//int d_tmp = 5;
-
+						
 								int x1 = j + l - w;
 								int x2 = x1 + d;
-								//int x2 = d_tmp + i + k - w;
-
-
+						
 								//check if pixel is within image
 								if (x1 >= 0 && x1 < width && x2 >= 0 && x2 < width) {
-
-									//Colors at position in window
-									Vec3b color1 = l_w_pixel[x2];
-									Vec3b color2 = r_w_pixel[x1];
-
-									//Absolute difference
-									//----
-									Vec3b v_diff;
-									absdiff(color1, color2, v_diff);
-									float diff = sum(v_diff)[0]; //Color difference
-
-
-									//Weights
-									//----
-									//Color differences: pixel at window pos to center pixel
-									//Euclidean Diff in CIELab color space
-
+									/* --------------------------- */
 									Vec3b col1 = l_w_pixel_LAB[x2];
 									Vec3b col2 = r_w_pixel_LAB[x1];
 
+									// computing color difference for left and right image - paper eq. 5
 									float d_cl = sqrt(col1[0] * l_color_LAB[0] + col1[1] * l_color_LAB[1] + col1[2] * l_color_LAB[2]);
 									float d_cr = sqrt(col2[0] * r_color_LAB[0] + col2[1] * r_color_LAB[1] + col2[2] * r_color_LAB[2]);
-
-
-	
-
-									//Spatial difference: distance from window pos to center
+							
+									// d_p / gamma_p is now precomputed
 									float d_p = distMap.at<float>(k, l);
 
-									// d_p / Gamma_p is now precomputed
-									float w_l = exp(-((d_cl / gamma_c) + (d_p /*/ gamma_p*/)));
-									float w_r = exp(-((d_cr / gamma_c) + (d_p /*/ gamma_p*/)));
+									// strength of grouping by color similarity and strength of grouping by proximity
+									float w_l = exp(-((d_cl / gamma_c) + d_p)); // paper eq. 9
+									float w_r = exp(-((d_cr / gamma_c) + d_p));
+							
+									/* --------------------------- */
+									//Colors at position in window
+									Vec3b color1 = l_w_pixel[x2];
+									Vec3b color2 = r_w_pixel[x1];
+							
+									//Absolute difference
+									Vec3b v_diff;
+									absdiff(color1, color2, v_diff);
+									// pixel-based raw matching cost computed by using the colors of q and ¯qd. When using AD (absolute difference)
+									float e_0 = sum(v_diff)[0]; //Color difference - Eq. 11
 
+									// The dissimilarity between pixel p in left image and p' in right image - Eq. 10
 									float weight = w_l * w_r;
+							
+									sum_v += (weight * e_0); // numerator
+									sum_w += weight;		  // denominator
+							
+									/* --------------------------- */
+								} // end if check pixel is in window
+							} // end l
+						} // if (i + k - w) < height
+					} // end k
+				} // end if i + d < width
 
-									sum_w += weight;
-									sum_v += (weight * diff);
-								}
-							}
-						}
-					}
-				}
 				float cost = 0.0f;
 				if (sum_w > 0.0f) {
-					cost = (sum_v / sum_w);
+					cost = (sum_v / sum_w); // Eq. 10 - The dissimilarity between pixel p and ¯pd,
 				}
 				//printf("cost = %.5f\n", cost);
 				costR.at<float>(i, j) = cost;
 				if ((j + d) < width) // Otherwise out of range exception
 					costL.at<float>(i, j + d) = cost;
-			}
-		}
+
+
+			} // end j
+		} // end i
 		costVolumeLeft.at(d) = costL;
 		costVolumeRight.at(d) = costR;
 		
-		printf("Finished for Disparity %d\n", d); // Just for debugging purpose
-	}
+		printf("Finished for Disparity %d\n", d);
+	} // end d
+
 
 }
 catch ( cv::Exception & e )
@@ -163,18 +162,7 @@ catch ( cv::Exception & e )
  cerr << e.msg << endl; // output exception message
 }
 
-
 }
-
-
-Vec3b CostVolume::colorAt(const cv::Mat &img, int y, int x) {
-//	unsigned char* input = (unsigned char*)img.data;
-//	Vec3b vec(Vec3b(input[img.step*x + y], input[img.step*x + y + 1], input[img.step*x + y + 2]));
-//	return vec;
-	return img.at<Vec3b>(y, x); //More efficient way didn't work.
-}
-
-
 
 CostVolume::CostVolume()
 {
--- Exercise3/opencv_test/opencv_test/main.cpp
+++ Exercise3/opencv_test/opencv_test/main.cpp
@@ -2,15 +2,27 @@
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
+#include <limits>
 #include <opencv2\opencv.hpp>
 #include "CostVolume.h"
 
 using namespace cv;
 using namespace std;
 
+void evaluation(cv::Mat &disparityL, cv::Mat &disparityR, int maxDisp, int windowSize);
+void setClosestDisparityValue(cv::Mat &newDisp, cv::Mat &oldDisp, int i, int j, int d, int &id_close_left, int id_close_right);
+void saveAndShowDisparityMaps(cv::Mat disparityL, cv::Mat disparityR, int maxDisp, int windowSize, std::string name);
+void refineDisparity(cv::Mat &dispLeft, cv::Mat &dispRight, int scaleDispFactor);
+
 int main()
 {
 	string filepath = "..\\images\\";
+
+	int windowSize = 5; // 5
+	int maxDisp = 15; // 15
+
+	if (false) {
+
 	//IplImage* img = cvLoadImage("..\\images\\tsukuba_left.png");
 	//Mat img1 = Mat(img);
 	//img = cvLoadImage("..\\images\\tsukuba_right.png");
@@ -21,14 +33,14 @@ int main()
 	//imshow("Left", img1);
 	//imshow("Right", img2);
 
-	int windowSize = 15; // 5
-	int maxDisp = 15; // 15
 
 	// Compute Cost Volume
 	vector<Mat> costVolumeLeft(maxDisp); // Need to allocate memory
 	vector<Mat> costVolumeRight(maxDisp);
 	CostVolume::computeCostVolume(img1, img2, costVolumeLeft, costVolumeRight, windowSize, maxDisp);
 
+
+	// Winner Takes it All - Get final Disparity Maps
 	Mat1i disparityL(img1.rows, img1.cols, 0);
 	Mat1i disparityR(img1.rows, img1.cols, 0);
 
@@ -46,7 +58,7 @@ int main()
 					float val = cvL.at<float>(i, j);
 					if (min == -1 || val < min) {
 						minL.at<float>(i, j) = val;
-						disparityL.at<int>(i, j) = d;
+						disparityL.at<int>(i, j) = d; // Eq. 12 - Winner-Takes-All
 					}
 					//right;
 					min = minR.at<float>(i, j);
@@ -63,54 +75,244 @@ int main()
 			}
 		}
 	}
+	
+	// imshow and save
+	saveAndShowDisparityMaps(disparityL, disparityR, maxDisp, windowSize, "DisparityMapWithoutRefine");
+		
+	// Task 2 - Refine Disparity Map
+	int scaleDispFactor = (255 / maxDisp); // is not used!
+	refineDisparity(disparityL, disparityR, scaleDispFactor);
+	
+	// evaluation
+	evaluation(disparityL, disparityR, maxDisp, windowSize);
 
-	disparityL *= (255 / maxDisp); //Should be same depth-colors as groundTruth
-	//disparityR *= (255 / maxDisp);
+	// imshow and save
+	saveAndShowDisparityMaps(disparityL, disparityR, maxDisp, windowSize, "Refined");
 
-	//cv::normalize(disparityL, disparityL, 0, 255, NORM_MINMAX);
-	//cv::normalize(disparityR, disparityR, 0, 255, NORM_MINMAX);
+	} // end if just for debbuging refineDisparity
+	else 
+	{   // Just for Debug reasons if on Task 2 Refine disparity is worked on
+		// Read in already computed disparity maps
+		std::string fnameL = filepath + "DisparityMapWithoutRefineLeft_w"  + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
+		std::string fnameR = filepath + "DisparityMapWithoutRefineRight_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
+	
+		Mat dispL = imread(fnameL);
+		cv::imshow("Disparity Mat Left", dispL);
+		Mat dispR = imread(fnameR);
+		cv::imshow("Disparity Mat Right", dispR);
+	
+		// Compute back to output of computeCostVolume
+		int width = dispL.cols;	int height = dispL.rows;
+		Mat1i disparityL(height, width, -1);
+		Mat1i disparityR(height, width, -1);
+		// Take only first dimension
+		for(int i=0; i < height; i++)
+		{
+			const Vec3b* r_pixel = dispR.ptr<Vec3b>(i);
+			const Vec3b* l_pixel = dispL.ptr<Vec3b>(i);
+			for (int j = 0; j < width; j++)
+			{
+				Vec3b d_r = r_pixel[j];
+				Vec3b d_l = l_pixel[j];
+				disparityR.at<int>(i,j) = d_r[0];
+				disparityL.at<int>(i,j) = d_l[0];
+			}
+		}
+		disparityR = (disparityR * maxDisp) / 255;
+		disparityL = (disparityL * maxDisp) / 255;
 
-	//imshow("Left Disparity Map", disparityL);
-	std::string fnameL = filepath + "DisparityMapLeft_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
-	//std::string fnameR = filepath + "DisparityMapRight_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
-	cv::imwrite(fnameL, disparityL);
-	//cv::imwrite(fnameR, disparityR);
+		// Task 2 - Refine Disparity Map
+		int scaleDispFactor = 5;
+		refineDisparity(disparityL, disparityR, scaleDispFactor);
 
-	Mat disMap1 = imread(fnameL);
-	imshow("Disparity Mat Left", disMap1);
-	//Mat disMap2 = imread(fnameR);
-	//imshow("Disparity Mat Right", disMap2);
+		// imshow and save
+		saveAndShowDisparityMaps(disparityL, disparityR, maxDisp, windowSize, "Refined");
 
+		// evaluation
+		evaluation(disparityL, disparityR, maxDisp, windowSize);
+	}	
 
-	/*
-	//--------------------------
-	//Evaluation
-	//---------------
+	cv::waitKey(0);
+	return 0;
+}
+
+void evaluation(cv::Mat &disparityL, cv::Mat &disparityR, int maxDisp, int windowSize) {
+	
+	string filepath = "..\\images\\";
+
 	Mat img_gt = imread(filepath + "tsukuba_gt.png", CV_LOAD_IMAGE_GRAYSCALE);
 	//Crop images to ignore borders
 	Rect R(Point(18,18), Point(366, 270)); //Create a rect 
 	Mat1b m_gt = img_gt(R);
 	Mat1b m_dL = disparityL(R);
-	//Mat1b m_dR = disparityR(R);
+	Mat1b m_dR = disparityR(R);
 
 	Mat1b m_eL = abs(m_gt - m_dL);
-	//Mat1b m_eR = abs(m_gt - m_dR);
+	Mat1b m_eR = abs(m_gt - m_dR);
 
 	threshold(m_eL, m_eL, (255 / maxDisp), 255, THRESH_BINARY);
-	//threshold(m_eR, m_eR, (255 / maxDisp), 255, THRESH_BINARY);
+	threshold(m_eR, m_eR, (255 / maxDisp), 255, THRESH_BINARY);
 
-	fnameL = filepath + "ErrorMapLeft_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
-	//fnameR = filepath + "ErrorMapRight_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
+	// Get sum of all incorrect pixels
+	int sum_left  = (cv::sum( m_eL )[0]) / 255;
+	int sum_right = (cv::sum( m_eR )[0]) / 255;
+	printf("Sum of incorret Pixels in Left  Image: %i\n",sum_left);
+	printf("Sum of incorret Pixels in Right Image: %i\n",sum_right);
+
+	std::string fnameL = filepath + "ErrorMapLeft_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
+	std::string fnameR = filepath + "ErrorMapRight_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
 	cv::imwrite(fnameL, m_eL);
-	//cv::imwrite(fnameR, m_eR);
+	cv::imwrite(fnameR, m_eR);
+	
+	Mat disMap1 = imread(fnameL);
+	cv::imshow("Error Mat Left", disMap1);
+	Mat disMap2 = imread(fnameR);
+	cv::imshow("Error Mat Right", disMap2);
+}
 
-	disMap1 = imread(fnameL);
-	imshow("Error Mat Left", disMap1);
-	//disMap2 = imread(fnameR);
-	//imshow("Error Mat Right", disMap2);
+void refineDisparity(cv::Mat &dispLeft, cv::Mat &dispRight, int scaleDispFactor) {
+		
+	int width = dispLeft.cols;
+	int height = dispLeft.rows;
+	
+	Mat1i markedDispL(height, width, -1);
+	Mat1i markedDispR(height, width, -1);
 
-	*/
+	for (int i = 0; i < height; i++) {
+		const int* r_pixel = dispRight.ptr<int>(i); // point to first pixel in row
+		const int* l_pixel = dispLeft.ptr<int>(i);
 
-	cv::waitKey(0);
-	return 0;
+		for (int j = 0; j < width; j++) {
+			try {
+				int d_r = r_pixel[j];
+
+				if ((j + d_r) < width)
+				{
+					int d_l = l_pixel[j + d_r];
+
+					//printf("r_color: %d\n",d_r);
+					//printf("l_color: %d\n",d_l);
+
+					// Check for inconsitent pixels by comparing each pixel from one image 
+					//		with its corresponding pixel in the other image.
+					// --> Mark those pixels as invalid whose corresponding disparity is 
+					//		different by a value larger than one pixel
+					if (abs(d_r - d_l) > 1) {
+						// Showing the error as blue - debbuging
+						//dispRight.at<Vec3b>(i,j) = Vec3b(15,0,0);
+						//dispLeft.at<Vec3b>(i,j + d_r) = Vec3b(15,0,0);
+
+						// This errors happen mostly in occluded or mismatched regions
+						markedDispR.at<int>(i,j)       = -1;
+						markedDispL.at<int>(i,j + d_r) = -1;
+
+					} else {
+						markedDispR.at<int>(i,j)       = d_r;
+						markedDispL.at<int>(i,j + d_r) = d_l;
+					}
+				}
+
+			}
+			catch (Exception & e)
+			{
+				cerr << e.msg << endl; // output exception message
+			}
+		}
+	}
+	//saveAndShowDisparityMaps(dispLeft, dispRight, 15, 5, "Error");
+
+	
+	// Fill those invalid pixels with the minimum of their closest 
+	//		valid left or right neighbor’s disparity
+	Mat1i refinedDispL(height, width, -1);
+	Mat1i refinedDispR(height, width, -1);
+
+	for (int i = 0; i < height; i++) {
+		const int* r_pixel = markedDispR.ptr<int>(i); // point to first pixel in row
+		const int* l_pixel = markedDispL.ptr<int>(i);
+
+		 // Save the position of pixel with valid disparity on the left of upcoming pixels
+		int r_id_close_left = -width;
+		int r_id_close_right = std::numeric_limits<int>::max();
+		int l_id_close_left = -width;
+		int l_id_close_right = std::numeric_limits<int>::max();
+
+		for (int j = 0; j < width; j++) {
+			int d_r = r_pixel[j];
+			int d_l = l_pixel[j];
+			try {
+				/* --------------------------- */
+				// Refine Disparity right image
+				setClosestDisparityValue(refinedDispR, markedDispR, i, j, d_r, r_id_close_left, r_id_close_right);
+			
+				/* --------------------------- */
+				// Refine Disparity left image
+				setClosestDisparityValue(refinedDispL, markedDispL, i, j, d_l, l_id_close_left, l_id_close_right);
+			}
+			catch (Exception & e)
+			{
+				cerr << e.msg << endl; // output exception message
+			}
+		}
+	}
+
+	// overwrite them so that they can be evaluated
+	dispLeft  = refinedDispL;
+	dispRight = refinedDispR;
+}
+
+/*
+* newDisp ... refinedMat
+* oldDisp ... marked with invalid pixels as -1
+* i,j     ... poistion
+* d       ... disparity value
+* id_close_left  ... holds the last valid disparity value left from j (and is updated)
+* id_close_right ... Integer max value
+*/
+void setClosestDisparityValue(cv::Mat &newDisp, cv::Mat &oldDisp, int i, int j, int d, int &id_close_left, int id_close_right) {
+	if (d == -1) { 
+		// neigbor on the left already saved
+		int id_left = id_close_left;
+
+		// look on the neighbors to the right
+		int id_right = id_close_right;
+		for (int j_r = j+1; j_r < oldDisp.cols; j_r++) { // go if necessary the whole width
+			if (oldDisp.at<int>(i,j_r) != -1) {
+				id_right = j_r;
+				break;
+			}
+		}
+
+		if (abs(j - id_left) <= abs(j - id_right)) {
+			// Take left neighbors disparity
+			newDisp.at<int>(i,j) = oldDisp.at<int>(i,id_left);
+		}
+		if (abs(j - id_left) >  abs(j - id_right)) {
+			// Take right neighbors disparity
+			newDisp.at<int>(i,j) = oldDisp.at<int>(i,id_right);
+		}
+	} else {
+		id_close_left = j; // Save the position of pixel with valid disparity on the left of upcoming pixels
+		newDisp.at<int>(i,j) = oldDisp.at<int>(i,j);
+	}
+}
+
+// Only for debugging reasons
+void saveAndShowDisparityMaps(cv::Mat disparityL, cv::Mat disparityR, int maxDisp, int windowSize, std::string name) {
+	
+	string filepath = "..\\images\\";
+	
+	// Compute Pixel Values to visualize again
+	disparityL *= (255 / maxDisp); //Should be same depth-colors as groundTruth
+	disparityR *= (255 / maxDisp);
+
+	std::string fnameL = filepath + name + "Left_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
+	std::string fnameR = filepath + name + "Right_w" + to_string(windowSize) + "_d" + to_string(maxDisp) + ".png";
+	cv::imwrite(fnameL, disparityL);
+	cv::imwrite(fnameR, disparityR);
+	
+	Mat disMap1 = imread(fnameL);
+	imshow(name + "Disparity Mat Left", disMap1);
+	Mat disMap2 = imread(fnameR);
+	imshow(name + "Disparity Mat Right", disMap2);
 }
